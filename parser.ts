290e9bf396e7aac4232a7dd06bc70a750baea04d
Sombian <sombian.software@gmail.com>
2024-06-24 21:01:59 +0900

    first commit

---
+++ parser.ts
@@ -0,0 +1,394 @@
+import Scanner, { Token } from "./scanner";
+
+abstract class AST
+{
+	public abstract parse(): string;
+}
+
+abstract class Branch extends AST
+{
+	constructor(public readonly parent: Branch, public readonly children: AST[] = [])
+	{
+		super();
+	}
+
+	public first()
+	{
+		return this.children[0];
+	}
+
+	public last()
+	{
+		return this.children[this.children.length - 1];
+	}
+}
+
+abstract class Leaf<T> extends AST
+{
+	constructor(public readonly data: T)
+	{
+		super();
+	}
+}
+//
+// core
+//
+class BREAK extends Leaf<never>
+{
+	override parse()
+	{
+		return `<br/>`;
+	}
+}
+//
+// block
+//
+class H1 extends Branch
+{
+	override parse()
+	{
+		return `<h1>${this.children.map((child) => child.parse()).join("")}</h1>`;
+	}
+}
+
+class H2 extends Branch
+{
+	override parse()
+	{
+		return `<h2>${this.children.map((child) => child.parse()).join("")}</h2>`;
+	}
+}
+
+class H3 extends Branch
+{
+	override parse()
+	{
+		return `<h3>${this.children.map((child) => child.parse()).join("")}</h3>`;
+	}
+}
+
+class H4 extends Branch
+{
+	override parse()
+	{
+		return `<h4>${this.children.map((child) => child.parse()).join("")}</h4>`;
+	}
+}
+
+class H5 extends Branch
+{
+	override parse()
+	{
+		return `<h5>${this.children.map((child) => child.parse()).join("")}</h5>`;
+	}
+}
+
+class H6 extends Branch
+{
+	override parse()
+	{
+		return `<h6>${this.children.map((child) => child.parse()).join("")}</h6>`;
+	}
+}
+
+class HR extends Branch
+{
+	override parse()
+	{
+		return `<hr/>`;
+	}
+}
+//
+// stack
+//
+class BQ extends Branch
+{
+	override parse()
+	{
+		return `<blockquote>${this.children.map((child) => child.parse()).join("")}</blockquote>`;
+	}
+}
+
+class OL extends Branch
+{
+	override parse()
+	{
+		return `<ol>${this.children.map((child) => child.parse()).join("")}</ol>`;
+	}
+}
+
+class UL extends Branch
+{
+	override parse()
+	{
+		return `<ul>${this.children.map((child) => child.parse()).join("")}</ul>`;
+	}
+}
+
+class LI extends Branch
+{
+	override parse()
+	{
+		return `<li>${this.children.map((child) => child.parse()).join("")}</li>`;
+	}
+}
+//
+// inline
+//
+class TEXT extends Leaf<{ value: string; bold?: boolean; italic?: boolean; underline?: boolean; strikethrough?: boolean; }>
+{
+	override parse()
+	{
+		const [tags, buffer] = [[] as string[], [] as string[]];
+
+		if (this.data.bold) tags.push("strong");
+		if (this.data.italic) tags.push("i");
+		if (this.data.underline) tags.push("u");
+		if (this.data.strikethrough) tags.push("s");
+
+		// open
+		for (const tag of tags)
+		{
+			buffer.push("<" + tag + ">");
+		}
+		// content
+		buffer.push(this.data.value);
+		// close
+		for (const tag of tags.reverse())
+		{
+			buffer.push("</" + tag + ">");
+		}
+
+		return buffer.join("");
+	}
+}
+
+class CHECKBOX extends Leaf<boolean>
+{
+	override parse()
+	{
+		return this.data ? `<input type="checkbox" checked/>` : `<input type="checkbox"/>`;
+	}
+}
+
+export default class Parser
+{
+	public static run(tokens: ReturnType<typeof Scanner.run>)
+	{
+		const origin = new (class ROOT extends Branch { override parse() { return `<article>${this.children.map((child) => child.parse()).join("")}</article>`; } })(null as never);
+
+		let node = origin;
+
+		let [bold, italic, underline, strikethrough] = [false, false, false, false];
+
+		let comment = false;
+
+		main:
+		for (const token of tokens)
+		{
+			if (comment)
+			{
+				switch (token)
+				{
+					case Token.COMMENT_L:
+					{
+						// ugh..?
+						break;
+					}
+					case Token.COMMENT_R:
+					{
+						comment = false;
+						break;
+					}
+				}
+				continue main;
+			}
+			switch (token)
+			{
+				//
+				// core
+				//
+				case Token.BREAK:
+				{
+					[bold, italic, underline, strikethrough] = [false, false, false, false];
+					
+					switch (node.constructor)
+					{
+						case H1:
+						case H2:
+						case H3:
+						case H4:
+						case H5:
+						case H6:
+						case HR:
+						{
+							break; // block-level, ignore
+						}
+						default:
+						{
+							node.children.push(new BREAK(null as never)); // others, continue
+							break;
+						}
+					}
+					node = origin
+					break;
+				}
+				case Token.COMMENT_L:
+				{
+					comment = true;
+					break;
+				}
+				case Token.COMMENT_R:
+				{
+					comment = false;
+					break;
+				}
+				//
+				// block
+				//
+				case Token.H1:
+				{
+					node.children.push(node = new H1(node));
+					break;
+				}
+				case Token.H2:
+				{
+					node.children.push(node = new H2(node));
+					break;
+				}
+				case Token.H3:
+				{
+					node.children.push(node = new H3(node));
+					break;
+				}
+				case Token.H4:
+				{
+					node.children.push(node = new H4(node));
+					break;
+				}
+				case Token.H5:
+				{
+					node.children.push(node = new H5(node));
+					break;
+				}
+				case Token.H6:
+				{
+					node.children.push(node = new H6(node));
+					break;
+				}
+				case Token.HR:
+				{
+					node.children.push(node = new HR(node));
+					break;
+				}
+				//
+				// stack
+				//
+				case Token.BLOCKQUOTE:
+				{
+					if (node.last() instanceof BQ)
+					{
+						node = node.last() as Branch;
+					}
+					else
+					{
+						node.children.push(node = new BQ(node));
+					}
+					break;
+				}
+				//
+				// layer
+				//
+				case Token.INDENT:
+				{
+					switch (node.last().constructor)
+					{
+						case OL:
+						case UL:
+						{
+							node = node.last() as Branch;
+							break;
+						}
+						default:
+						{
+							// fallback
+							node.children.push(new TEXT({ value: "\t" }));
+							break;
+						}
+					}
+					break;
+				}
+				case Token.OL:
+				{
+					if (node.last() instanceof OL)
+					{
+						node = node.last() as Branch;
+					}
+					else
+					{
+						node.children.push(node = new OL(node))
+					}
+					// insert
+					node.children.push(node = new LI(node));
+					break;
+				}
+				case Token.UL:
+				{
+					if (node.last() instanceof UL)
+					{
+						node = node.last() as Branch;
+					}
+					else
+					{
+						node.children.push(node = new UL(node))
+					}
+					// insert
+					node.children.push(node = new LI(node));
+					break;
+				}
+				//
+				// inline
+				//
+				case Token.BOLD:
+				{
+					bold = !bold;
+					break;
+				}
+				case Token.ITALIC:
+				{
+					italic = !italic;
+					break;
+				}
+				case Token.UNDERLINE:
+				{
+					underline = !underline;
+					break;
+				}
+				case Token.STRIKETHROUGH:
+				{
+					strikethrough = !strikethrough;
+					break;
+				}
+				case Token.UNCHECKED_BOX:
+				{
+					node.children.push(new CHECKBOX(false));
+					break;
+				}
+				case Token.CHECKED_BOX:
+				{
+					node.children.push(new CHECKBOX(true));
+					break;
+				}
+				default:
+				{
+					if (typeof token === "string")
+					{
+						node.children.push(new TEXT({ value: token, bold, italic, underline, strikethrough }));
+					}
+					break;
+				}
+			}
+		}
+		return origin;
+	}
+}
---
+++ scanner.ts
@@ -0,0 +1,473 @@
+const enum Context
+{
+	BLOCK = "block", // Block
+	NEST = "nest", // Nest extends Block
+	LIST = "list", // List extends Nest
+	INLINE = "inline", // Inline
+}
+
+export abstract class Token
+{
+	public readonly rules: string[];
+
+	private constructor(...rules: string[])
+	{
+		this.rules = rules;
+	}
+
+	public static of(ctx: Context)
+	{
+		switch (ctx)
+		{
+			case Context.BLOCK:
+			{
+				return [
+					// core
+					Token.BREAK,
+					Token.COMMENT_L,
+					Token.COMMENT_R,
+					// block
+					Token.H1,
+					Token.H2,
+					Token.H3,
+					Token.H4,
+					Token.H5,
+					Token.H6,
+					Token.HR,
+					// stack
+					Token.BLOCKQUOTE,
+					// layer
+					Token.INDENT,
+					Token.OL,
+					Token.UL,
+					// inline
+					Token.BOLD,
+					Token.ITALIC,
+					Token.UNDERLINE,
+					Token.STRIKETHROUGH,
+					Token.UNCHECKED_BOX,
+					Token.CHECKED_BOX,
+				];
+			}
+			case Context.NEST:
+			{
+				return [
+					// core
+					Token.BREAK,
+					Token.COMMENT_L,
+					Token.COMMENT_R,
+					// stack
+					Token.BLOCKQUOTE,
+					// layer
+					Token.INDENT,
+					Token.OL,
+					Token.UL,
+					// inline
+					Token.BOLD,
+					Token.ITALIC,
+					Token.UNDERLINE,
+					Token.STRIKETHROUGH,
+					Token.UNCHECKED_BOX,
+					Token.CHECKED_BOX,
+				];
+			}
+			case Context.LIST:
+			{
+				return [
+					// core
+					Token.BREAK,
+					Token.COMMENT_L,
+					Token.COMMENT_R,
+					// stack
+					Token.BLOCKQUOTE,
+					// layer
+					Token.INDENT,
+					Token.OL,
+					Token.UL,
+					// inline
+					Token.BOLD,
+					Token.ITALIC,
+					Token.UNDERLINE,
+					Token.STRIKETHROUGH,
+					Token.UNCHECKED_BOX,
+					Token.CHECKED_BOX,
+				];
+			}
+			case Context.INLINE:
+			{
+				return [
+					// core
+					Token.BREAK,
+					Token.COMMENT_L,
+					Token.COMMENT_R,
+					// inline
+					Token.BOLD,
+					Token.ITALIC,
+					Token.UNDERLINE,
+					Token.STRIKETHROUGH,
+					Token.UNCHECKED_BOX,
+					Token.CHECKED_BOX,
+				];
+			}
+		}
+	}
+	public abstract get ctx(): Context;
+	//
+	// core
+	//
+	public static readonly BREAK = new (class BREAK extends Token
+	{
+		override get ctx(): Context
+		{
+			throw new Error();
+		}
+	})
+	("\n");
+	public static readonly COMMENT_L = new (class COMMENT_L extends Token
+	{
+		override get ctx(): Context
+		{
+			throw new Error();
+		}
+	})
+	("/*");
+	public static readonly COMMENT_R = new (class COMMENT_R extends Token
+	{
+		override get ctx(): Context
+		{
+			throw new Error();
+		}
+	})
+	("*/");
+	//
+	// block
+	//
+	public static readonly H1 = new (class H1 extends Token
+	{
+		override get ctx()
+		{
+			return Context.BLOCK;
+		}
+	})
+	("#\u0020");
+	public static readonly H2 = new (class H2 extends Token
+	{
+		override get ctx()
+		{
+			return Context.BLOCK;
+		}
+	})
+	("##\u0020");
+	public static readonly H3 = new (class H3 extends Token
+	{
+		override get ctx()
+		{
+			return Context.BLOCK;
+		}
+	})
+	("###\u0020");
+	public static readonly H4 = new (class H4 extends Token
+	{
+		override get ctx()
+		{
+			return Context.BLOCK;
+		}
+	})
+	("####\u0020");
+	public static readonly H5 = new (class H5 extends Token
+	{
+		override get ctx()
+		{
+			return Context.BLOCK;
+		}
+	})
+	("#####\u0020");
+	public static readonly H6 = new (class H6 extends Token
+	{
+		override get ctx()
+		{
+			return Context.BLOCK;
+		}
+	})
+	("######\u0020");
+	public static readonly HR = new (class HR extends Token
+	{
+		override get ctx()
+		{
+			return Context.BLOCK;
+		}
+	})
+	("---", "===");
+	//
+	// stack
+	//
+	public static readonly BLOCKQUOTE = new (class BLOCKQUOTE extends Token
+	{
+		override get ctx()
+		{
+			return Context.NEST;
+		}
+	})
+	(">\u0020");
+	//
+	// layer
+	//
+	public static readonly INDENT = new (class INDENT extends Token
+	{
+		override get ctx()
+		{
+			return Context.LIST;
+		}
+	})
+	("	", "  ", "    ");
+	public static readonly OL = new (class ORDERED_LIST extends Token
+	{
+		override get ctx()
+		{
+			return Context.LIST;
+		}
+	})
+	("-\u0020");
+	public static readonly UL = new (class UNORDERED_LIST extends Token
+	{
+		override get ctx()
+		{
+			return Context.LIST;
+		}
+	})
+	("~\u0020");
+	//
+	// inline
+	//
+	public static readonly BOLD = new (class BOLD extends Token
+	{
+		override get ctx()
+		{
+			return Context.INLINE;
+		}
+	})
+	("**");
+	public static readonly ITALIC = new (class ITALIC extends Token
+	{
+		override get ctx()
+		{
+			return Context.INLINE;
+		}
+	})
+	("??");
+	public static readonly UNDERLINE = new (class UNDERLINE extends Token
+	{
+		override get ctx()
+		{
+			return Context.INLINE;
+		}
+	})
+	("__");
+	public static readonly STRIKETHROUGH = new (class STRIKETHROUGH extends Token
+	{
+		override get ctx()
+		{
+			return Context.INLINE;
+		}
+	})
+	("~~");
+	public static readonly UNCHECKED_BOX = new (class UNCHECKED_BOX extends Token
+	{
+		override get ctx()
+		{
+			return Context.INLINE;
+		}
+	})
+	("[ ]");
+	public static readonly CHECKED_BOX = new (class CHECKED_BOX extends Token
+	{
+		override get ctx()
+		{
+			return Context.INLINE;
+		}
+	})
+	("[x]");
+}
+
+interface Route
+{
+	[key: string]: Token | Route;
+}
+
+const __TABLE__: Record<Context, Route> =
+{
+	// auto generate
+	[Context.BLOCK]: {},
+	// auto generate
+	[Context.NEST]: {},
+	// auto generate
+	[Context.LIST]: {},
+	// auto generate
+	[Context.INLINE]: {},
+};
+
+for (const ctx of [Context.BLOCK, Context.NEST, Context.LIST, Context.INLINE])
+{
+	for (const token of Token.of(ctx))
+	{
+		for (const rule of token.rules)
+		{
+			let node = __TABLE__[ctx];
+
+			for (let i = 0; i < rule.length; i++)
+			{
+				const char = rule[i];
+
+				if (i + 1 === rule.length)
+				{
+					node[char] = token; 
+				}
+				else
+				{
+					// @ts-ignore
+					node = node[char] ?? (node[char] = {});
+				}
+			}
+		}
+	}
+}
+
+export default class Scanner
+{
+	private constructor()
+	{
+		// final
+	}
+	
+	public static run(input: string)
+	{
+		const [tokens, buffer] = [[] as (string | Token)[], [] as string[]];
+
+		let [ctx, node, depth, escape] = [Context.BLOCK, null as (null | Route), 0, false];
+
+		main:
+		for (const char of input.replace(/\r\n?/g, "\n"))
+		{
+			//
+			// STEP 1. escape
+			//
+			if (!escape && char === "\\")
+			{
+				//
+				// STEP 2. reset
+				//
+				[ctx, node, depth, escape] = [Context.INLINE, null, 0, true];
+
+				continue main;
+			}
+			//
+			// STEP 2. consume
+			//
+			buffer.push(char);
+			//
+			// STEP 3. unescape
+			//
+			if (escape)
+			{
+				//
+				// STEP 4. reset
+				//
+				[ctx, node, depth, escape] = [Context.INLINE, null, 0, false];
+
+				continue main;
+			}
+			//
+			// STEP 4. delve branch
+			//
+			if (char in (node ??= __TABLE__[ctx]))
+			{
+				//
+				// STEP 5. into the deep
+				//
+				depth++
+				//
+				// STEP 6. examine token
+				//
+				if (node[char] instanceof Token)
+				{
+					//
+					// STEP 7. switch ctx
+					//
+					if ([Token.BREAK, Token.COMMENT_L, Token.COMMENT_R].includes(node[char]))
+					{
+						// core -> inline
+						ctx = Context.BLOCK;
+					}
+					else
+					{
+						switch (node[char].ctx)
+						{
+							case Context.BLOCK:
+							{
+								// block -> inline
+								ctx = Context.INLINE;
+								break;
+							}
+							case Context.NEST:
+							{
+								// nest -> nest
+								ctx = Context.NEST;
+								break;
+							}
+							case Context.LIST:
+							{
+								// list -> list
+								ctx = Context.LIST;
+								break;
+							}
+							case Context.INLINE:
+							{
+								// inline -> inline
+								ctx = Context.INLINE;
+								break;
+							}
+						}
+					}
+					//
+					// STEP 8. flush buffer
+					//
+					if (0 < buffer.length - depth)
+					{
+						tokens.push(buffer.join("").slice(0, 0 < depth ? buffer.length - depth : Infinity));
+					}
+					//
+					// STEP 9. build token
+					//
+					tokens.push(node[char]);
+					//
+					// STEP 10. reset
+					//
+					[node, depth, buffer.length] = [null, 0, 0];
+				}
+				else
+				{
+					//
+					// STEP 7. delve branch
+					//
+					node = node[char];
+				}
+			}
+			else
+			{
+				//
+				// STEP 5. reset
+				//
+				[ctx, node, depth] = [Context.INLINE, null, 0];
+			}
+		}
+		//
+		// STEP ?. flush buffer
+		//
+		if (0 < buffer.length)
+		{
+			tokens.push(buffer.join(""));
+		}
+		
+		return tokens;
+	}
+}
